456dc164390c9bd5fe765bf0549047e5822f70d2
Peter Akala <40690295+petecs@users.noreply.github.com>
2019-08-23 00:31:06 -0700

    created Card class, tested relative import, tested Card class

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from model.Card import Card
+
+def main():
+    test = Card("Ace", 4)
+    print(test)
+
+main()
---
+++ src/model/Card.py
@@ -0,0 +1,9 @@
+import random
+
+class Card:
+    def __init__(self, suit, value):
+        self.__suit = suit
+        self.__value = value
+    
+    def __repr__(self):
+        return " of ".join((str(self.__value), self.__suit))
