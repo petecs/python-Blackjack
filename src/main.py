92dd1ba9e0308930526b3f9f8b4ae7d2f9ed13b5
Peter Akala <40690295+petecs@users.noreply.github.com>
2019-08-23 01:40:40 -0700

    removed public access to list of cards in Deck class (for testing only)

--- src/main.py
+++ src/main.py
@@ -2,9 +2,6 @@ from model import Card
 from model import Deck
 
 def main():
-    test_card = Card("Ace", "10")
-    print(test_card)
-    test_deck = Deck()
-    print(test_deck.cards)
+    pass
 
 main()
--- src/model/deck.py
+++ src/model/deck.py
@@ -6,7 +6,6 @@ class Deck:
         self.__values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         
         self.__cards = []
-        self.cards = self.__cards
 
         for suit in self.__suits:
             for value in self.__values:
